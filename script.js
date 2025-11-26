2540968a4dee923ecec450a6581643b027f6ead2
Agentmrc <157666271+Agentmrc@users.noreply.github.com>
2025-11-26 22:32:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ document.getElementById('resetView').addEventListener('click', () => {
   iframe.src = '';   // force reload
   setTimeout(() => iframe.src = src, 50);
 });
+
+// List of tours with button IDs, URLs, and labels
+const tours = [
+  { id: 'tourA-btn', url: 'https://agentmrc.github.io/virtual-tour-project/tourA/', label: 'Tour A' },
+  { id: 'tourB-btn', url: 'https://agentmrc.github.io/virtual-tour-project/tourB/', label: 'Tour B' },
+  { id: 'tourC-btn', url: 'https://agentmrc.github.io/virtual-tour-project/tourC/', label: 'Tour C' },
+  { id: 'tourD-btn', url: 'https://agentmrc.github.io/virtual-tour-project/tourD/', label: 'Tour D' },
+  { id: 'tourE-btn', url: 'https://agentmrc.github.io/virtual-tour-project/tourE/', label: 'Tour E' },
+];
+
+tours.forEach(tour => {
+  const button = document.getElementById(tour.id);
+  if(button) {
+    button.addEventListener('click', () => {
+      // GA4 event
+      gtag('event', 'click_tour', {
+        'event_category': 'virtual_tour',
+        'event_label': tour.label
+      });
+      // Redirect to virtual tour
+      window.location.href = tour.url;
+    });
+  }
+});
+
